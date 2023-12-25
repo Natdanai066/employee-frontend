ae64d627921a1dc50361d63e08cd8fa4e7b85d93
Natdanai066 <s63122201066@ssru.ac.th>
2023-12-25 15:10:06 +0700

    update page.js

--- my-pay-slip/src/app/editSlip/page.js
+++ my-pay-slip/src/app/editSlip/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import Axios from 'axios';
+import Axios from '../axios';
 import { useSearchParams } from "next/navigation"
 import Link from 'next/link'
 
