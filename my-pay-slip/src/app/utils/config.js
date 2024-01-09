68a35eed78acf6da46e4007cf191800531b22a96
Natdanai066 <s63122201066@ssru.ac.th>
2024-01-09 18:57:34 +0700

    update server

--- my-pay-slip/src/app/utils/config.js
+++ my-pay-slip/src/app/utils/config.js
@@ -1,6 +1,6 @@
 
 const config = {
-     SERVER_URL: 'employee1-backend-production.up.railway.app',
+     SERVER_URL: 'https://employee1-backend-production.up.railway.app',
     // SERVER_URL: 'http://localhost:3001',
 }
 
